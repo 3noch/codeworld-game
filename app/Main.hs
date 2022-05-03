87f47ceb93eedb5be67c8423a7a2cdc91f58bd17
Elliot Cameron <eacameron@gmail.com>
2022-05-03 11:29:43 -0400

    Reorganize

--- app/Main.hs
+++ app/Main.hs
@@ -12,6 +12,18 @@ import Control.Applicative
 import Data.List (foldl', sortOn)
 import Data.Foldable
 
+main = activityOf world0 update draw
+
+world0 = World {
+    worldPlayer = Sprite person 0 10 0 0 5 4 MovementLogicStationary,
+    worldArea = level1Area,
+    worldDt = 0
+  }
+
+level1Area = mkArea grass $ areaStr level1
+level2Area = mkArea dirt $ areaStr level2
+level3Area = mkArea dirt $ areaStr level3
+
 level1 =
   ["                                           ",
    "                                           ",
@@ -93,77 +105,7 @@ level3 =
    "WWWWWWWWWWWWWWWWWWWWWWWWWggggggWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
    "WWWWWWWWWWWWWWWWWWWWWWWWWggggggWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
    " 1                                                        ❤❤             2  "
-   ]
-
-data Thing = Thing {
-    thingPic :: Picture,
-    thingWidth :: Double,
-    thingHeight :: Double,
-    thingOpaque :: Bool,
-    thingCollisionRect :: Maybe (Rect Double),
-    thingCollisionBehavior :: CollisionBehavior
-  }
-
-data CollisionBehavior
-  = CollisionBlock
-  | CollisionApply (World -> World) Bool
-
-data Sprite = Sprite {
-    spriteThing :: Thing,
-    spriteX :: Double,
-    spriteY :: Double,
-    spriteVelocityX :: Double,
-    spriteVelocityY :: Double,
-    spriteHealth :: Double,
-    spriteMaxVelocity :: Double,
-    spriteMovement :: MovementLogic
-  }
-data MovementLogic = MovementLogicStationary | MovementLogicGoToPlayer
-
-data World = World {
-    worldPlayer :: Sprite,
-    worldArea :: Area,
-    worldDt :: Double
-  }
-
-data Area = Area {
-    areaTiles :: Map (Double, Double) LocationContent,
-    areaSprites :: [Sprite],
-    areaWidth :: Int,
-    areaHeight :: Int,
-    areaDefaultTile :: Thing
-  }
-
-data LocationContent = LocationContent {
-    locationContentTile :: Maybe Thing, -- Opaque tile at this location
-    locationContentThings :: [Thing]
-  }
-
-data Rect a = Rect { rectLeft :: a, rectTop :: a, rectRight :: a, rectBottom :: a } deriving Eq
-rectCenterX (Rect l _ r _) = l + abs (l - r) / 2
-rectCenterY (Rect _ t _ b) = t - abs (b - t) / 2
-rectArea (Rect l t r b) = abs (r - l) * abs (b - t)
-(.+) (x, y) (Rect l t r b) = Rect (l + x) (t + y) (r + x) (b + y)
-
-mapInsert :: Double -> Double -> Thing -> Map (Double, Double) LocationContent -> Map (Double, Double) LocationContent
-mapInsert x y thing = Map.insertWith
-  (\(LocationContent newTile newThings) (LocationContent oldTile oldThings) ->
-    LocationContent (newTile <|> oldTile) (newThings ++ oldThings)
-  )
-  (y, x)
-  (if thingOpaque thing then LocationContent (Just thing) [] else LocationContent Nothing [thing])
-
-main = activityOf world0 update draw
-
-level1Area = mkArea grass $ areaStr level1
-level2Area = mkArea dirt $ areaStr level2
-level3Area = mkArea dirt $ areaStr level3
-
-world0 = World {
-    worldPlayer = Sprite person 0 10 0 0 5 4 MovementLogicStationary,
-    worldArea = level1Area,
-    worldDt = 0
-  }
+  ]
 
 tree1 = (mkThing "Tree" "https://github.com/3noch/codeworld-game/raw/main/img/tree1.png" 2 3) { thingCollisionRect = Just (Rect 0.4 (-0.8) 1.2 (-3)) }
 grass = (mkThing "Grass" "https://github.com/3noch/codeworld-game/raw/main/img/grass.png" 1.1 1.1) { thingOpaque = True, thingCollisionRect = Nothing }
@@ -205,23 +147,6 @@ parseMapChar '2' = Just $ Right $ portalTo level2Area 0 0
 parseMapChar '3' = Just $ Right $ portalTo level3Area (-15) (-5)
 parseMapChar _ = Nothing
 
-areaStr = map (map parseMapChar)
-
-mkArea :: Thing -> [[Maybe (Either Sprite Thing)]] -> Area
-mkArea defaultTile rows = foldl' (\m (x, y, maybeThing) -> maybe m (add m x y) maybeThing) (Area Map.empty [] width height defaultTile) indexed
-  where
-    add m x y (Left sprite) = m { areaSprites = sprite { spriteX = x, spriteY = y } : areaSprites m }
-    add m x y (Right thing) = m { areaTiles = mapInsert x y thing (areaTiles m) }
-    indexed = concat $
-      for (height `div` 2) (-1) rows $ \(y, columns) ->
-        for (-width `div` 2) 1 columns $ \(x, thing) -> (fromIntegral x, fromIntegral y, thing)
-    for start step xs f = zipWith (curry f) [start, start+step..] xs
-    height = length rows
-    width = maximum $ map length rows
-
-mkThing :: Text -> Text -> Double -> Double -> Thing
-mkThing name url w h = Thing (image name url w h) w h False (Just $ Rect 0 0 w (-h)) CollisionBlock
-
 draw world | spriteHealth (worldPlayer world) > 0 = drawAlive world
 draw _ = drawDead
 
@@ -255,7 +180,85 @@ drawAlive world = frameRate & healthHearts & translated (-rectCenterX viewPort) 
     col = collision (spriteCollisionRect $ worldPlayer world) [(x, y, loc) | ((y, x), loc) <- Map.toAscList $ worldArea world]
     -}
 
+mapInsert :: Double -> Double -> Thing -> Map (Double, Double) LocationContent -> Map (Double, Double) LocationContent
+mapInsert x y thing = Map.insertWith
+  (\(LocationContent newTile newThings) (LocationContent oldTile oldThings) ->
+    LocationContent (newTile <|> oldTile) (newThings ++ oldThings)
+  )
+  (y, x)
+  (if thingOpaque thing then LocationContent (Just thing) [] else LocationContent Nothing [thing])
+
 mkAreaThings area = foldl' (\m sprite -> mapInsert (spriteX sprite) (spriteY sprite) (spriteThing sprite) m) (areaTiles area) (areaSprites area)
+
+-- Types --
+data Thing = Thing {
+    thingPic :: Picture,
+    thingWidth :: Double,
+    thingHeight :: Double,
+    thingOpaque :: Bool,
+    thingCollisionRect :: Maybe (Rect Double),
+    thingCollisionBehavior :: CollisionBehavior
+  }
+mkThing :: Text -> Text -> Double -> Double -> Thing
+mkThing name url w h = Thing (image name url w h) w h False (Just $ Rect 0 0 w (-h)) CollisionBlock
+
+data CollisionBehavior
+  = CollisionBlock
+  | CollisionApply (World -> World) Bool
+
+data Sprite = Sprite {
+    spriteThing :: Thing,
+    spriteX :: Double,
+    spriteY :: Double,
+    spriteVelocityX :: Double,
+    spriteVelocityY :: Double,
+    spriteHealth :: Double,
+    spriteMaxVelocity :: Double,
+    spriteMovement :: MovementLogic
+  }
+data MovementLogic = MovementLogicStationary | MovementLogicGoToPlayer
+
+data World = World {
+    worldPlayer :: Sprite,
+    worldArea :: Area,
+    worldDt :: Double
+  }
+
+data Area = Area {
+    areaTiles :: Map (Double, Double) LocationContent,
+    areaSprites :: [Sprite],
+    areaWidth :: Int,
+    areaHeight :: Int,
+    areaDefaultTile :: Thing
+  }
+areaStr = map (map parseMapChar)
+
+mkArea :: Thing -> [[Maybe (Either Sprite Thing)]] -> Area
+mkArea defaultTile rows = foldl' (\m (x, y, maybeThing) -> maybe m (add m x y) maybeThing) (Area Map.empty [] width height defaultTile) indexed
+  where
+    add m x y (Left sprite) = m { areaSprites = sprite { spriteX = x, spriteY = y } : areaSprites m }
+    add m x y (Right thing) = m { areaTiles = mapInsert x y thing (areaTiles m) }
+    indexed = concat $
+      for (height `div` 2) (-1) rows $ \(y, columns) ->
+        for (-width `div` 2) 1 columns $ \(x, thing) -> (fromIntegral x, fromIntegral y, thing)
+    for start step xs f = zipWith (curry f) [start, start+step..] xs
+    height = length rows
+    width = maximum $ map length rows
+
+
+data LocationContent = LocationContent {
+    locationContentTile :: Maybe Thing, -- Opaque tile at this location
+    locationContentThings :: [Thing]
+  }
+
+--   Rect
+data Rect a = Rect { rectLeft :: a, rectTop :: a, rectRight :: a, rectBottom :: a } deriving Eq
+rectCenterX (Rect l _ r _) = l + abs (l - r) / 2
+rectCenterY (Rect _ t _ b) = t - abs (b - t) / 2
+rectArea (Rect l t r b) = abs (r - l) * abs (b - t)
+(.+) (x, y) (Rect l t r b) = Rect (l + x) (t + y) (r + x) (b + y)
+isOverlapping (Rect l0 t0 r0 b0) (Rect l1 t1 r1 b1) = l0 < r1 && r0 > l1 && t0 > b1 && b0 < t1
+rectOverlap a@(Rect l0 t0 r0 b0) b@(Rect l1 t1 r1 b1) = if isOverlapping a b then Just (Rect (max l0 l1) (min t0 t1) (min r0 r1) (max b0 b1)) else Nothing
 
 limitViewPort (Rect ml mt mr mb) (Rect l t r b) = Rect l' t' r' b'
   where
@@ -267,6 +270,14 @@ limitViewPort (Rect ml mt mr mb) (Rect l t r b) = Rect l' t' r' b'
 tileRect :: Rect Double -> Rect Double
 tileRect (Rect l t r b) = Rect (fromIntegral $ floor l) (fromIntegral $ ceiling t) (fromIntegral $ ceiling r) (fromIntegral $ floor b)
 
+boundingRect :: (Double, Double) -> Rect Double -> Rect Double
+boundingRect (x, y) (Rect l t r b) = Rect l' t' (l' + w) (t' - h)
+  where
+    w = abs (r - l)
+    h = abs (b - t)
+    l' = x + l - w/2
+    t' = y + t + h/2
+
 collision1 :: Rect Double -> Rect Double -> (Double, Double)
 collision1 rect1@(Rect l0 t0 r0 b0) rect2@(Rect l1 t1 r1 b1) = if colliding then adjustment else (0, 0)
   where
@@ -276,17 +287,6 @@ collision1 rect1@(Rect l0 t0 r0 b0) rect2@(Rect l1 t1 r1 b1) = if colliding then
     adjustment = case leastChange of
       (x, False) -> (x, 0)
       (y, True) -> (0, y)
-
-isOverlapping (Rect l0 t0 r0 b0) (Rect l1 t1 r1 b1) = l0 < r1 && r0 > l1 && t0 > b1 && b0 < t1
-rectOverlap a@(Rect l0 t0 r0 b0) b@(Rect l1 t1 r1 b1) = if isOverlapping a b then Just (Rect (max l0 l1) (min t0 t1) (min r0 r1) (max b0 b1)) else Nothing
-
-boundingRect :: (Double, Double) -> Rect Double -> Rect Double
-boundingRect (x, y) (Rect l t r b) = Rect l' t' (l' + w) (t' - h)
-  where
-    w = abs (r - l)
-    h = abs (b - t)
-    l' = x + l - w/2
-    t' = y + t + h/2
 
 buildCollisions :: Rect Double -> [(Double, Double, LocationContent)] -> (Double, Double, World -> World)
 buildCollisions r m = applyCollisionsTo r
@@ -311,9 +311,6 @@ drawRect :: Rect Double -> Picture
 drawRect (Rect l t r b) = polyline points
   where
     points = [(l, t), (r, t), (r, b), (l, b), (l, t)]
-
-round' :: Double -> Double
-round' x = fromIntegral (round (x * 10) :: Int) / 10
 
 update (KeyPress "Esc") w = world0
 update event w | spriteHealth (worldPlayer w) > 0 = handle event w
